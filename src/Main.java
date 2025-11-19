28052921d61e13b921050658dae190e5af0fefb5
kaynanmelo <kaynan.melo@gec.inatel.br>
2025-11-19 13:48:53 -0300

    importado as pastas na Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import br.producao.excecoes.maquinaQuebradaException;
+import br.producao.maquinas.Maquina;
+import br.producao.maquinas.MaquinaCorte;
+import br.producao.maquinas.MaquinaInspecao;
+import br.producao.maquinas.MaquinaMontagem;
+import br.producao.produtos.Produto;
+import br.producao.produtos.Registravel;
+import br.producao.simulacao.LinhaProducao;
+
+
 public class Main {
     public static void main(String[] args) {
-
+        MaquinaCorte m1 = new MaquinaCorte("serralheria", 5);
     }
 }
